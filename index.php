a58d978a35ce0a4f1c3a64a1203077bf3a7ed3e3
Maria Anikina <maria-anik@mail.ru>
2020-02-09 20:31:02 +0300

    set autoload composer - fix2

--- index.php
+++ index.php
@@ -1,35 +1,12 @@
 <?
-<<<<<<< HEAD
 require_once "vendor/autoload.php";
-use taskforse\Task;
+use taskforse\Task2;
 
 
 echo "hello</br></br>";
-$arrList = Task::TaskStatusList();
+$arrList = Task2::TaskStatusList();
 foreach ($arrList as $item) {
 	echo $item;
 	echo "</br>";
 }
 ?>
-=======
-use HtmlAcademy\Task as Task;
-require_once 'vendor/autoload.php';
-
-
-$list_status = Task::taskStatusList();
-$list_action = Task::taskActionList();
-
-foreach($list_status as $item) {
-    echo $item.'<br>';
-}
-echo "<br> <br>";
-
-foreach($list_action as $item) {
-    echo $item.'<br>';
-}
-
-
-?>
-
-
->>>>>>> module1-task2
